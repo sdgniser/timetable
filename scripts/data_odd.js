61f487edf4a26f44fb832858d03b782ca5992fe1
Ayush Singh <ayush.singh@niser.ac.in>
2019-07-24 08:53:52 +0530

    added course names

--- scripts/data_odd.js
+++ scripts/data_odd.js
@@ -2,112 +2,139 @@ let courseList = {
 	"sbs": {
 		"B201": {
 			"slot": "D",
+			"name": "Microbiology",
 		},
 
 		"B202": {
 			"slot": "A",
+			"name":  "Biochemistry",
 		},
 
 		"B203": {
 			"slot": "F",
+			"name":  "Biophysics and Biostatistics",
 		},
 
 		"B301": {
 			"slot": "D",
+			"name": "Physiology I (Animal)",
 		},
 
 		"B302": {
 			"slot": "E",
+			"name": "Physiology II (Plant)",
 		},
 
 		"B303": {
 			"slot": "F",
+			"name": "Ecology",
 		},
 
 		"B351": {
 			"slot": "C",
+			"name": "Principles of Drug Design",
 		},
 
 		"B354": {
 			"slot": "A",
+			"name": "Neurobiology",
 		},
 
 		"B401": {
 			"slot": "F",
+			"name": "Genetic Engineering",
 		},
 
 		"B402": {
 			"slot": "A",
+			"name": "Developmental Biology",
 		},
 
 		"B451": {
 			"slot": "E",
+			"name": "Advanced Cell Biology",
 		},
 
 		"B452": {
 			"slot": "C",
+			"name": "Biotechniques",
 		},
 
 		"B460": {
 			"slot": "D",
+			"name": "Virology",
 		},
 
 		"B552": {
 			"slot": "A",
+			"name": "Advanced Immunology",
 		},
 	},
 	
 	"sps": {
 		"P201": {
 			"slot": "D",
+			"name": "Classical Mechanics",
 		},
 
 		"P202": {
 			"slot": "E",
+			"name": "Mathematical Methods I",
 		},
 
 		"P203": {
 			"slot": "F",
+			"name": "Electronics",
 		},
 
 		"P302": {
 			"slot": "A",
+			"name": "Statistical Mechanics",
 		},
 
 		"P303": {
 			"slot": "F",
+			"name": "Quantum Mechanics II",
 		},
 
 		"P304": {
 			"slot": "E",
+			"name": "Special Theory of Relativity",
 		},
 
 		"P307": {
 			"slot": "J",
+			"name": "Nuclei and Particles",
 		},
 
 		"P401": {
 			"slot": "E",
+			"name": "Classical Mechanics II",
 		},
 
 		"P451": {
 			"slot": "C",
+			"name": "Advanced Solid State Physics",
 		},
 
 		"P453": {
 			"slot": "J",
+			"name": "Quantum Field Theory I",
 		},
 
 		"P454": {
 			"slot": "D",
+			"name": "Particle Physics",
 		},
 
 		"P471": {
 			"slot": "A",
+			"name": "Quantum Information and Quantum Computation",
 		},
 
 		"P474": {
-			"slot": "F"
+			"slot": "F",
+			"name": "Introduction to Cosmology",
 		},
 
 	},
@@ -115,58 +142,72 @@ let courseList = {
 	"sms": {
 		"M201": {
 			"slot": "B",
+			"name": "Real Analysis",
 		},
 
 		"M202": {
 			"slot": "C",
+			"name": "Group Theory"
 		},
 
 		"M203": {
 			"slot": "F",
+			"name": "Discrete Mathematics",
 		},
 
 		"M207": {
 			"slot": "E",
+			"name": "Number Theory",
 		},
 
 		"M302": {
 			"slot": "J",
+			"name": "Rings and Modules",
 		},
 
 		"M303": {
 			"slot": "B",
+			"name": "Differential Equations",
 		},
 
 		"M304": {
 			"slot": "A",
+			"name": "Topology",
 		},
 
 		"M305": {
 			"slot": "C",
+			"name": "Statistics",
 		},
 
 		"M306": {
 			"slot": "E",
+			"name": "Calculus of Several Variables",
 		},
 
 		"M401": {
 			"slot": "D",
+			"name": "Functional Analysis",
 		},
 
 		"M403": {
 			"slot": "J",
+			"name": "Commutative Algebra",
 		},
 
 		"M451": {
 			"slot": "G",
+			"name": "Advanced Complex Analysis",
 		},
 
 		"M453": {
-			"slot": "I"
+			"slot": "I",
+			"name": "Advanced Linear Algebra",
 		},
 
 		"M555": {
-			"slot": "H"
+			"slot": "H",
+			"name": "Harmonic Analysis"
 		},
 
 	},
@@ -174,50 +215,62 @@ let courseList = {
 	"scs": {
 		"C201": {
 			"slot": "B",
+			"name": "Basic Inorganic Chemistry",
 		},
 
 		"C207": {
 			"slot": "F",
+			"name": "Mathematical Methods for Chemists",
 		},
 
 		"C203": {
 			"slot": "C",
+			"name": "Reaction Mechanism in Organic Chemistry",
 		},
 
 		"C301": {
 			"slot": "A",
+			"name": "Physical Organic Chemistry",
 		},
 
 		"C302": {
 			"slot": "E",
+			"name": "Molecular Spectroscopy and Group Theory",
 		},
 
 		"C307": {
 			"slot": "B",
+			"name": "Thermodynamics and Electrochemistry",
 		},
 
 		"C401": {
 			"slot": "B",
+			"name": "Physical Methods in Chemistry II",
 		},
 
 		"C403": {
 			"slot": "C",
+			"name": "Chemistry of Heterocycles and Natural Products",
 		},
 
 		"C557": {
 			"slot": "I",
+			"name": "Nuclear Magnetic Resonance",
 		},
 
 		"C559": {
-			"slot": "G"
+			"slot": "G",
+			"name": "Supramolecular Chemistry",
 		},
 
 		"C566": {
-			"slot": "J"
+			"slot": "J",
+			"name": "Catalysis: Reaction Mechanisms and Applications"
 		},
 
 		"C571": {
-			"slot": "H"
+			"slot": "H",
+			"name": "Statistical Mechanics",
 		},
 
 	},
@@ -225,18 +278,22 @@ let courseList = {
 	"scss": {
 		"CS201": {
 			"slot": "K",
+			"name": "Theory of Computation",
 		},
 
 		"CS301": {
 			"slot": "K",
+			"name": "Design and Analysis of Algorithms",
 		},
 
 		"CS451": {
 			"slot": "K",
+			"name": "Modern Cryptology",
 		},
 
 		"CS454": {
 			"slot": "B",
+			"name": "Linear Programming and Combinatorial Optimization",
 		},
 
 	},
